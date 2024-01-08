4d2fad8441c7de7047b3d7b89d8ea6db3fee0a16
anthonyagb <78220962+anthony-agb@users.noreply.github.com>
2024-01-08 12:27:52 +0100

    Feat(Payment): Add auth & database configuration.

--- GrubHubClone.Payment/DataAccess/DatabaseContext.cs
+++ GrubHubClone.Payment/DataAccess/DatabaseContext.cs
@@ -5,18 +5,7 @@ namespace GrubHubClone.Payment.DataAccess;
 
 public class DatabaseContext : DbContext
 {
-    protected readonly IConfiguration Configuration;
-
-    public DatabaseContext(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // in memory database used for simplicity, change to a real db for production applications
-        options.UseInMemoryDatabase("TestDb");
-    }
+    public DatabaseContext(DbContextOptions options) : base(options) { }
 
     public DbSet<PaymentModel> Payments { get; set; }
 }
